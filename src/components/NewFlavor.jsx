f5a6bd1f0cd8c56ef63c88338c4183b0df9c3224
SaiNawaratTun <sainawarattun123@gmail.com>
2024-01-26 13:48:34 +0630

    update

--- src/components/NewFlavor.jsx
+++ src/components/NewFlavor.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 export default function NewRelease() {
 
     return (
-        <div className='w-full h-[200px] md:h-[400px] px-2 md:px-24'>
-            <div className="carousel h-full w-full px-8 md:px-16 gap-3 ">
+        <div className='w-full h-[200px] md:h-[400px] pl-2 md:pl-24'>
+            <div className="carousel h-full w-full md:px-12 gap-3 ">
                 <div id="slide1" className="carousel-item relative w-full border rounded-lg">
                     <img src="https://s42695.pcdn.co/wp-content/uploads/2023/04/electronic-cogarette-vape-on-a-dark-background-with-steam.jpeg" className="w-full border rounded-lg " />
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2 ">
-                        <a href="#slide4" className="btn btn-circle w-8 h-16 md:h-40 transition duration-300 border rounded-lg border-slate-700" >❮</a>
+                    <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 top-1/2 ">
+                        {/* <a href="#slide4" className="btn btn-circle w-8 h-16 md:h-40 transition duration-300 border rounded-lg border-slate-700" >❮</a> */}
                         <a href="#slide2" className="btn btn-circle w-8 h-16 md:h-40 transition duration-300 border rounded-lg border-slate-700">❯</a>
                     </div>
                     <div className='absolute inset-0 ml-10 flex items-center justify-end mr-24 md:mr-40 space-y-3'>
@@ -21,8 +21,8 @@ export default function NewRelease() {
                 </div>
                 <div id="slide2" className="carousel-item relative w-full">
                     <img src="https://img.freepik.com/premium-photo/colorful-disposable-electronic-cigarettes-with-shadows-black-background-concept-modern-smoking-vaping-nicotine_126267-4215.jpg" className="w-full border rounded-lg" />
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2 ">
-                        <a href="#slide1" className="btn btn-circle w-8 h-16 md:h-40 transition duration-300 border rounded-lg border-slate-700">❮</a>
+                    <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 top-1/2 ">
+                        {/* <a href="#slide1" className="btn btn-circle w-8 h-16 md:h-40 transition duration-300 border rounded-lg border-slate-700">❮</a> */}
                         <a href="#slide3" className="btn btn-circle w-8 h-16 md:h-40 transition duration-300 border rounded-lg border-slate-700">❯</a>
                     </div>
                     <div className='absolute inset-0 ml-10 flex items-center justify-end mr-24 md:mr-40 space-y-3'>
@@ -35,8 +35,8 @@ export default function NewRelease() {
                 </div>
                 <div id="slide3" className="carousel-item relative w-full">
                     <img src="https://img.pikbest.com/ai/illus_our/20230529/6abc1479a404bec21f678539736231f9.jpg!w700wp" className="w-full border rounded-lg" />
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide2" className="btn btn-circle w-8 h-16 md:h-40 transition duration-300 border rounded-lg border-slate-700">❮</a>
+                    <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        {/* <a href="#slide2" className="btn btn-circle w-8 h-16 md:h-40 transition duration-300 border rounded-lg border-slate-700">❮</a> */}
                         <a href="#slide4" className="btn btn-circle w-8 h-16 md:h-40 transition duration-300 border rounded-lg border-slate-700">❯</a>
                     </div>
                     <div className='absolute inset-0 ml-10 flex items-center justify-end mr-24 md:mr-40 space-y-3'>
@@ -49,8 +49,8 @@ export default function NewRelease() {
                 </div>
                 <div id="slide4" className="carousel-item relative w-full">
                     <img src="https://png.pngtree.com/background/20230524/original/pngtree-many-different-colored-smoke-pens-arranged-on-a-black-floor-picture-image_2712086.jpg" className="w-full border rounded-lg" />
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide3" className="btn btn-circle w-8 h-16 md:h-40 transition duration-300 border rounded-lg border-slate-700">❮</a>
+                    <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        {/* <a href="#slide3" className="btn btn-circle w-8 h-16 md:h-40 transition duration-300 border rounded-lg border-slate-700">❮</a> */}
                         <a href="#slide1" className="btn btn-circle w-8 h-16 md:h-40 transition duration-300 border rounded-lg border-slate-700">❯</a>
                     </div>
                     <div className='absolute inset-0 ml-10 flex items-center justify-end mr-24 md:mr-40 space-y-3'>
